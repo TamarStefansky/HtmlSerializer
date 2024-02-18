a96e1790620ec9366387f5f28c43ef75f1211686
TamarStefansky <ts058320350@gmail.com>
2024-02-18 23:49:05 +0200

    first commit

---
+++ Practicod2/HtmlElement.cs
@@ -0,0 +1,126 @@
+﻿using Practicod2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicode2
+{
+    internal class HtmlElement
+    {
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<string> Attributes { get; set; }
+        public List<string> Classes { get; set; }
+        public string InnerHtml { get; set; }
+        public HtmlElement Parent { get; set; }
+        public List<HtmlElement> Children { get; set; }
+
+        public HtmlElement()
+        {
+            Attributes = new List<string>();
+            Classes = new List<string>();
+            Children = new List<HtmlElement>();
+        }
+
+        public IEnumerable<HtmlElement> Descendants()
+        {
+            Queue<HtmlElement> queue = new Queue<HtmlElement>();
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+                HtmlElement current = queue.Dequeue();
+                yield return current;
+
+                foreach (var child in current.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+        }
+
+        public IEnumerable<HtmlElement> Ancestors()
+        {
+            HtmlElement current = this;
+            while (current != null)
+            {
+                yield return current;
+                current = current.Parent;
+            }
+        }
+
+        public HashSet<HtmlElement> FindElementsBySelector(Selector selector)
+        {
+            HashSet<HtmlElement> resultSet = new HashSet<HtmlElement>();
+            FindElementsBySelectorRecursive(this, selector, resultSet);
+            return resultSet;
+        }
+
+        private void FindElementsBySelectorRecursive(HtmlElement element, Selector selector, HashSet<HtmlElement> resultSet)
+        {
+            if (selector == null)
+            {
+                return;
+            }
+
+            IEnumerable<HtmlElement> descendants = element.Descendants();
+            foreach (HtmlElement descElement in descendants)
+            {
+
+                if (MatchesSelector(descElement, selector))
+                {
+                    resultSet.Add(descElement);
+
+                    if (selector.Child != null)
+                    {
+                        FindElementsBySelectorRecursive(descElement, selector.Child, resultSet);
+                    }
+                }
+            }
+        }
+
+        private bool MatchesSelector(HtmlElement element, Selector selector)
+        {
+            if (selector.TagName != null && selector.TagName != "" && element.Name != selector.TagName)
+            {
+                return false;
+            }
+
+            if (selector.Id != null && element.Id != selector.Id)
+            {
+                return false;
+            }
+
+            if (selector.Classes != null && element.Classes != null && !selector.Classes.All(c => element.Classes.Contains(c)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Practicod2/Program.cs
@@ -0,0 +1,168 @@
+﻿using Practicod2;
+using Practicode2;
+using System.Text.RegularExpressions;
+
+var html = await Load("https://learn.malkabruk.co.il/");
+
+static async Task<string> Load(string url)
+{
+    HttpClient client = new HttpClient();
+    var response = await client.GetAsync(url);
+    var html = await response.Content.ReadAsStringAsync();
+    return html;
+}
+
+HtmlElement rootElement = Buildtree(html);
+
+PrintTree(rootElement, 0);
+
+static HtmlElement Buildtree(string html)
+{
+    var clearHtml = new Regex(@"[\r\n]+").Replace(html, "");
+    var htmlLines = new Regex("<(.*?)>").Split(clearHtml).Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
+    int keywordIndex = htmlLines.FindIndex(s => Regex.IsMatch(s, @"^\s*html"));
+    htmlLines.RemoveRange(0, keywordIndex+1 );
+    HtmlElement rootElement = new HtmlElement { Name = "html", Children = new List<HtmlElement>(), Parent = null };
+    HtmlElement currentElement = rootElement;
+    int currentIndex = 0;
+
+    while (currentIndex < htmlLines.Count)
+    {
+        string line = htmlLines[currentIndex];
+        if (line.StartsWith("!--"))
+        {
+            while (!line.Contains("-->"))
+            {
+                currentIndex++;
+                line = htmlLines[currentIndex];
+            }
+            continue;
+        }
+        if (line == "/html")
+        {
+            break;
+        }
+        var attributes = new Regex("([^\\s]*?)=\"(.*?)\"").Matches(line).Cast<Match>()
+            .Select(match => match.Value).ToList();
+        
+
+        int firstSpace = line.IndexOf(' ');
+        string name, id = null, cls = null;
+        List<string> classes = null;
+
+        if (firstSpace == -1)
+        {
+            name = line;
+        }
+        else
+            name = line.Substring(0, line.IndexOf(" "));
+
+        if (attributes.Count > 0)
+        {
+            id = attributes.FirstOrDefault(attr => attr.StartsWith("id"));
+            if (id != null)
+            {
+                attributes.RemoveAll(attr => attr == id);
+                id = id.Substring(4);
+            }
+
+            cls = attributes.FirstOrDefault(attr => attr.StartsWith("class"));
+            if (cls != null)
+            {
+                attributes.RemoveAll(attr => attr == cls);
+                cls = cls.Substring(7);
+                classes = cls.Split(" ").ToList();
+            }
+        }
+
+        
+        if (line.StartsWith("/"))
+        {
+            if (currentElement.Parent != null)
+            {
+                currentElement = currentElement.Parent;
+            }
+            else
+            {
+                Console.WriteLine("There is no parent");
+            }
+        }
+
+        else if (!HtmlHelper.Instance.isHtmlTag(name))
+        {
+            currentElement.InnerHtml = (string)line;
+        }
+        else if (HtmlHelper.Instance.IsSelfClosingTag(name))
+        {
+            HtmlElement child = new HtmlElement
+            {
+                Name = name,
+                Id = id,
+                Classes = classes,
+                Attributes = attributes,
+                Parent = currentElement
+            };
+            currentElement.Children.Add(child);
+        }
+        else
+        {
+            HtmlElement child = new HtmlElement
+            {
+                Name = name,
+                Id = id,
+                Classes = classes,
+                Attributes = attributes,
+                Parent = currentElement,
+                Children = new List<HtmlElement>()
+            };
+
+
+            currentElement.Children.Add(child);
+            currentElement = child;
+        }
+
+        currentIndex++;
+    }
+    return rootElement;
+}
+
+static void PrintTree(HtmlElement element, int depth)
+{
+    Console.WriteLine(new string(' ', depth * 2) + element.Name);
+
+    foreach (var child in element.Children)
+    {
+        PrintTree(child, depth + 1);
+    }
+}
+
+
+string query = "div #mydiv .class-name";
+Selector selectorObject = Selector.FromQueryString(query);
+Print(selectorObject);
+// הדפסת האובייקט באמצעות המתודה Print
+void Print(Selector selector)
+{
+    if (selector != null)
+    {
+        Console.Write(selector.ToString());
+
+        if (selector.Child != null)
+        {
+            Console.Write(" > ");
+            Print(selector.Child);
+        }
+        else
+        {
+            Console.WriteLine(); // לשורה חדשה כשאין ילד
+        }
+    }
+}
+
+
+
+
+
+
+
+
